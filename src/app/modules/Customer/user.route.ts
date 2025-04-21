003e853600530f160f936a8bcc6c7433c5341929
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-04-21 22:56:02 +0600

    user/customer validation and route created

---
+++ src/app/modules/Customer/user.route.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { customerController } from "./user.controller";
+import { validRequest } from "../../middlewares/validRequest";
+import { createCustomerValidation, updateCustomerValidation } from "./user.validation";
+
+const router = Router()
+
+router.post('/',
+    validRequest(createCustomerValidation),
+    customerController.customerCreate
+)
+
+router.get('/', customerController.getAllCustomer)
+
+router.get('/:customerId', customerController.getSingleCustomer)
+
+router.put('/:customerId',
+    validRequest(updateCustomerValidation),
+    customerController.updateCustomer
+)
+
+router.delete('/:customerId', customerController.deleteACustomer)
+
+export const customerRoutes = router
