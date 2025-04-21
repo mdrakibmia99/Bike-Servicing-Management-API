e3480ad5cea1b01d950c7ea89ef5640d5e4ec1e5
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-04-21 22:56:48 +0600

    services router and validation created

---
+++ src/app/modules/Service/service.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { ServiceController } from "./service.controller";
+
+const router = Router()
+
+router.post('/',  ServiceController.ServiceCreate)
+
+router.get('/', ServiceController.getAllServices)
+
+router.get('/status', ServiceController.serviceStatus)
+
+router.get('/:ServiceId', ServiceController.getSingleService)
+
+router.put('/:ServiceId/complete',  ServiceController.serviceCompleted)
+
+router.patch('/:ServiceId',  ServiceController.updateService)
+
+router.delete('/:ServiceId', ServiceController.deleteService)
+
+export const serviceRoutes = router
